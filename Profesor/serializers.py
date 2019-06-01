918c059a2038e4a7634db48f2bd471306729ed14
AngelRuiz99 <181109@ids.upchiapas.edu.mx>
2019-06-01 14:48:42 -0500

    first commit

--- Profesor/serializers.py
+++ Profesor/serializers.py
@@ -1,12 +1,10 @@
-# ----------------librerias------------
 from rest_framework import routers, serializers, viewsets
 
-# ----------------Modelos--------------
-# Nombre app                      nombre modelo
+
 from Profesor.models import Profesor
 
 from drf_dynamic_fields import DynamicFieldsMixin
-#                         serializers
+
 class ProfesorSerializers(DynamicFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = Profesor
